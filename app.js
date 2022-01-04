8c17ffe166454634917fb7bd71fdbb519e579e33
Victor Wei <victoryqwei@gmail.com>
2022-01-03 22:21:13 -0500

    Cleaned up code in app.js

--- app.js
+++ app.js
@@ -73,8 +73,6 @@ app.use(function (req, res, next) {
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/*', function (req, res, next) {
-	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-	console.log(ip);
 	res.redirect('/')
 	next()
 })
