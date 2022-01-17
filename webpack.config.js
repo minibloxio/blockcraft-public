9e2cc1605645c15112cf71318926cea22ef0828f
Howard Yun <haoy2001@gmail.com>
2022-01-17 03:24:04 -0500

    Skip the menus when developing

--- webpack.config.js
+++ webpack.config.js
@@ -4,60 +4,63 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
-module.exports = {
-    entry: './public/src/app.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+module.exports = (env, argv) => {
+    return {
+        entry: './public/src/app.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist'),
         },
-        compress: true,
-        port: 3001,
-        proxy: {
-            '/socket.io': {
-                target: 'http://localhost:3002',
-                ws: true
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+            compress: true,
+            port: 3001,
+            proxy: {
+                '/socket.io': {
+                    target: 'http://localhost:3002',
+                    ws: true
+                }
+            },
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Cross-Origin-Embedder-Policy": "require-corp",
+                "Cross-Origin-Opener-Policy": "same-origin"
             }
         },
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Cross-Origin-Embedder-Policy": "require-corp",
-            "Cross-Origin-Opener-Policy": "same-origin"
-        }
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+        module: {
+            rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+
+                test: /\.(png|otf)$/,
+                type: 'asset/resource'
+
+            }
+            ],
         },
-        {
-
-            test: /\.(png|otf)$/,
-            type: 'asset/resource'
-
-        }
+        plugins: [
+            new webpack.DefinePlugin({ 'DEV_MODE_SKIP_MENUS': argv.mode == 'development' }),
+            new FaviconsWebpackPlugin('./public/assets/favicon.png'),
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+                filename: "index.html",
+                path: path.resolve(__dirname, 'dist'),
+            }),
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+            }),
+            new CopyPlugin({
+                patterns: [{ from: "./public/textures", to: "./textures" }],
+            }),
+            new CopyPlugin({
+                patterns: [{ from: "./public/src/workers", to: "./src/workers" }],
+            }),
         ],
-    },
-    plugins: [
-        new FaviconsWebpackPlugin('./public/assets/favicon.png'),
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            filename: "index.html",
-            path: path.resolve(__dirname, 'dist'),
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-        }),
-        new CopyPlugin({
-            patterns: [{ from: "./public/textures", to: "./textures" }],
-        }),
-        new CopyPlugin({
-            patterns: [{ from: "./public/src/workers", to: "./src/workers" }],
-        }),
-    ],
 
-};
+    };
+}
