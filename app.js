c77cdf94f0526ba1bb32d53810e46a4e7ad20dd5
Howard Yun <haoy2001@gmail.com>
2022-01-17 23:06:30 -0500

    Fix Dockerfile

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function (req, res, next) {
 });
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/*', function (req, res, next) {
     res.redirect('/')
     next()
