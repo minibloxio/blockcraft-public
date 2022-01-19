20c51143f13a66e4427c425617a0ae341a030d4e
Howard Yun <haoy2001@gmail.com>
2022-01-18 23:07:09 -0500

    Fix 3d item lighting

--- public/src/graphics/ActiveItemScene.ts
+++ public/src/graphics/ActiveItemScene.ts
@@ -9,6 +9,9 @@ class ActiveItemScene {
     const hemi = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.75);
     hemi.position.set(0, 5000, 0);
     scene.add(hemi);
+    const dir = new THREE.DirectionalLight("white", 0.4);
+    dir.position.set(0, 4000, 0);
+    scene.add(dir);
   }
 }
 
