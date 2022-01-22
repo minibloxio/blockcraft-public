2c250cfdca51da1a0b0265a94c9e17cac7cd72b4
Howard Yun <haoy2001@gmail.com>
2022-01-21 23:27:54 -0500

    Refactor input system

--- public/src/input/input.ts
+++ public/src/input/input.ts
@@ -1,33 +1,36 @@
 import * as $ from "jquery";
-import { initKeys } from "kontra";
+import { initKeys, initPointer } from "kontra";
 import player from "../Player";
 
 import "./KeyboardInput";
 import "./MouseInput";
 
-initKeys();
+export function initInput() {
+  initKeys();
+  initPointer();
 
-$(window).on("keydown", function (event) {
-  if (!g.initialized) return;
-  if (!player.controls.enabled) return;
-  if (event.keyCode == 18) {
-    // alt
-    event.preventDefault();
-  }
-  if (event.altKey && event.keyCode == 68) {
-    // alt + d
-    event.preventDefault();
-  }
-  if (event.altKey && event.keyCode == 32) {
-    // alt + space
-    event.preventDefault();
-  }
-});
-
-// Creative menu search
-$(document).ready(function () {
-  $("#search-input").on("input", function () {
-    let search = $(this).val();
-    inventory.updateItemSearch(search);
+  $(window).on("keydown", function (event) {
+    if (!g.initialized) return;
+    if (!player.controls.enabled) return;
+    if (event.keyCode == 18) {
+      // alt
+      event.preventDefault();
+    }
+    if (event.altKey && event.keyCode == 68) {
+      // alt + d
+      event.preventDefault();
+    }
+    if (event.altKey && event.keyCode == 32) {
+      // alt + space
+      event.preventDefault();
+    }
   });
-});
+
+  // Creative menu search
+  $(document).ready(function () {
+    $("#search-input").on("input", function () {
+      let search = $(this).val();
+      inventory.updateItemSearch(search);
+    });
+  });
+}
